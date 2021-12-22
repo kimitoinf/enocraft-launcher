ee02bc976510dcf794a8919446b27dc435299678
limlnlog <limlnlog@gmail.com>
2021-12-22 20:00:21 +0900

    Add login message.

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ function createWindow()
 		}
 	}).on('end', function(end)
 	{
-		window.webContents.send('log-download-end', 'dummy');
+		window.webContents.send('log-scroll', 'dummy');
 		wget.download(server + "CocoaInput-1.17.1-forge-4.0.4.jar", path.join(os.tmpdir(), "CocoaInput-1.17.1-forge-4.0.4.jar"), {}).on('progress', function(progress)
 		{
 			var percent = Math.floor(progress * 100);
@@ -43,7 +43,7 @@ function createWindow()
 			}
 		}).on('end', function(end)
 		{
-			window.webContents.send('log-download-end', 'dummy');
+			window.webContents.send('log-scroll', 'dummy');
 			wget.download(server + "OptiFine_1.17.1_HD_U_H1_MOD.jar", path.join(os.tmpdir(), "OptiFine_1.17.1_HD_U_H1_MOD.jar"), {}).on('progress', function(progress)
 			{
 				var percent = Math.floor(progress * 100);
@@ -54,7 +54,7 @@ function createWindow()
 				}
 			}).on('end', function(end)
 			{
-				window.webContents.send('log-download-end', 'dummy');
+				window.webContents.send('log-scroll', 'dummy');
 				wget.download(server + "wthit-forge-3.11.2.jar", path.join(os.tmpdir(), "wthit-forge-3.11.2.jar"), {}).on('progress', function(progress)
 				{
 					var percent = Math.floor(progress * 100);
@@ -65,7 +65,7 @@ function createWindow()
 					}
 				}).on('end', function(end)
 				{
-					window.webContents.send('log-download-end', 'dummy');
+					window.webContents.send('log-scroll', 'dummy');
 					wget.download(server + "Xaeros_Minimap_FP21.22.3_Forge_1.17.1.jar", path.join(os.tmpdir(), "Xaeros_Minimap_FP21.22.3_Forge_1.17.1.jar"), {}).on('progress', function(progress)
 					{
 						var percent = Math.floor(progress * 100);
@@ -76,7 +76,7 @@ function createWindow()
 						}
 					}).on('end', function(end)
 					{
-						window.webContents.send('log-download-end', 'dummy');
+						window.webContents.send('log-scroll', 'dummy');
 						fs.exists(path.join(os.tmpdir(), 'openjdk-17.0.1_windows-x64_bin.zip'), function(exists)
 						{
 							if (!exists)
@@ -91,9 +91,10 @@ function createWindow()
 									}
 								}).on('end', function(end)
 								{
-									window.webContents.send('log-download-end', 'dummy');
+									window.webContents.send('log-scroll', 'dummy');
 								});
 							}
+							window.webContents.send('message-login', 'dummy');
 						});
 					});
 				});
--- src/renderer.js
+++ src/renderer.js
@@ -5,7 +5,12 @@ ipcRenderer.on('log-download', (event, data) =>
 	document.getElementById('Log').value += data + "\n";
 });
 
-ipcRenderer.on('log-download-end', (event, data) =>
+ipcRenderer.on('log-scroll', (event, data) =>
 {
 	document.getElementById('Log').scrollTop = document.getElementById('Log').scrollHeight;
+});
+
+ipcRenderer.on('message-login', (event, data) =>
+{
+	document.getElementById('Log').value += "All downloads completed. Log in." + "\n";
 });
